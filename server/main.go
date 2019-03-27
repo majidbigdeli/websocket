dc72e63104ae50bcb288c286d6d71f355a12caab
majid <majidbigdeli72@gmail.com>
2019-03-27 04:37:10 +0430

    Check Header

--- server/main.go
+++ server/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/kataras/iris"
 	"github.com/majidbigdeli/websocket/websocket"
 )
 
+var cho = func(r *http.Request) bool {
+	// allow all connections by default
+	dd := r.Header.Get("Upgrade")
+	fmt.Println(dd)
+	return true
+}
+
 func main() {
 	app := iris.New()
-	ws := websocket.New(websocket.Config{})
+	ws := websocket.New(websocket.Config{CheckOrigin: cho})
 	ws.OnConnection(func(c websocket.Connection) {
 		//go func() {
 		//	<-time.After(20 * time.Second)
